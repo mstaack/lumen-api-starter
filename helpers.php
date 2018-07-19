941ef039e2d72cb383bb6caf5d562de8c1e04c52
Max <maxmatteostaack@gmail.com>
2018-07-19 15:45:01 +0200

    remove polyfill

--- helpers.php
+++ helpers.php
@@ -1,13 +1,1 @@
 <?php
-if (!function_exists('config_path')) {
-    /**
-     * Get the configuration path.
-     *
-     * @param  string $path
-     * @return string
-     */
-    function config_path($path = '')
-    {
-        return app()->basePath() . DIRECTORY_SEPARATOR . 'config' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
-    }
-}
