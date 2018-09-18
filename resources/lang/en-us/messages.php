6e3779335864fdba584c1e28fbbba8f2821cc566
stefan <s.a.h.v.herwijnen@student.tue.nl>
2018-09-18 08:18:41 +0200

    Added translation messages

---
+++ resources/lang/en-us/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+// resources/lang/en/messages.php
+
+return [
+    'welcome_subject' => 'Please verify your account',
+    'welcome_header' => 'Welcome to :Website, :Name',
+    'welcome_text' => 'In order to complete your registration, you will have to verify your email address.',
+    'welcome_link' => 'Please click here to verify your account.',
+    'password_reset_subject' => 'You have requested to reset your password',
+    'password_reset_text' => 'You have requested to reset your password on :Website. Use the link below to set a new password.',
+    'password_reset_link' => 'Please click here in order to reset your password.',
+];
